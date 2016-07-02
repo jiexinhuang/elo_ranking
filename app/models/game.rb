1f23f28fc69707249bdc2090ad5a49e4cc7f8f19
Jiexin Huang <jiexin.huang@envato.com>
2016-07-02 21:12:38 +1000

    Add associations and basic validations for Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+  belongs_to :home_player, class_name: 'Player', foreign_key: :home_player_id
+  belongs_to :away_player, class_name: 'Player', foreign_key: :away_player_id
+
+  validates :game_type, presence: true
+  validates :result, presence: true
+  validates :home_player, presence: true
+  validates :away_player, presence: true
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :home_player }
+  it { is_expected.to belong_to :away_player }
+  it { is_expected.to validate_presence_of :game_type }
+  it { is_expected.to validate_presence_of :result }
+  it { is_expected.to validate_presence_of :home_player }
+  it { is_expected.to validate_presence_of :away_player }
 end
