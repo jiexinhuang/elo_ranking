0c1b5bedb2b382115ee5341d644d90fc2eed0926
Jiexin Huang <jiexin.huang@envato.com>
2016-07-09 12:42:24 +0930

    Add more config options

--- lib/elo/configuration.rb
+++ lib/elo/configuration.rb
@@ -1,7 +1,11 @@
 class Elo::Configuration
   include ActiveSupport::Configurable
 
-  config_accessor(:default_rating) { 1000 }
-  config_accessor(:pro_rating_boundry) { 2000 }
-  config_accessor(:starter_boundry) { 20 }
+  config_accessor(:default_rating) { 1500 }
+  config_accessor(:starter_games_boundary) { 20 }
+  config_accessor(:pro_rating_boundary) { 2500 }
+
+  config_accessor(:starter_k_factor) { 25 }
+  config_accessor(:default_k_factor) { 15 }
+  config_accessor(:pro_k_factor) { 10 }
 end
--- spec/lib/elo/configuration_spec.rb
+++ spec/lib/elo/configuration_spec.rb
@@ -3,20 +3,29 @@ require 'rails_helper'
 RSpec.describe Elo::Configuration do
   subject(:configuration) { described_class.new }
   describe 'default configuration values' do
-    its(:default_rating) { is_expected.to eq 1000 }
-    its(:pro_rating_boundry) { is_expected.to eq 2000 }
-    its(:starter_boundry) { is_expected.to eq 20 }
+    its(:default_rating) { is_expected.to eq 1500 }
+    its(:pro_rating_boundary) { is_expected.to eq 2500 }
+    its(:starter_games_boundary) { is_expected.to eq 20 }
+    its(:starter_k_factor) { is_expected.to eq 25 }
+    its(:default_k_factor) { is_expected.to eq 15 }
+    its(:pro_k_factor) { is_expected.to eq 10 }
   end
 
   describe 'use custom config' do
     before do
       configuration.config.default_rating = 2000
-      configuration.config.pro_rating_boundry = 3000
-      configuration.config.starter_boundry = 40
+      configuration.config.pro_rating_boundary = 3000
+      configuration.config.starter_games_boundary = 40
+      configuration.config.starter_k_factor= 50
+      configuration.config.default_k_factor = 30
+      configuration.config.pro_k_factor = 20
     end
 
     its(:default_rating) { is_expected.to eq 2000 }
-    its(:pro_rating_boundry) { is_expected.to eq 3000 }
-    its(:starter_boundry) { is_expected.to eq 40 }
+    its(:pro_rating_boundary) { is_expected.to eq 3000 }
+    its(:starter_games_boundary) { is_expected.to eq 40 }
+    its(:starter_k_factor) { is_expected.to eq 50 }
+    its(:default_k_factor) { is_expected.to eq 30 }
+    its(:pro_k_factor) { is_expected.to eq 20 }
   end
 end
