e10045be768938383aa209fa863d05d61e41413a
Jiexin Huang <jiexin.huang@envato.com>
2016-07-09 20:35:39 +0930

    List players by rating

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
     @game_type = params[:game_type]
-    @players = Player.where(game_type: @game_type)
+    @players = Player.where(game_type: @game_type).order('rating desc')
   end
 end
