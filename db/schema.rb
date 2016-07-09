3b2406eb52e45d573bbe91c715078c95175b3804
Jiexin Huang <jiexin.huang@envato.com>
2016-07-09 20:36:58 +0930

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627223937) do
+ActiveRecord::Schema.define(version: 20160709110611) do
 
   create_table "games", force: :cascade do |t|
     t.string   "game_type"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20160627223937) do
     t.integer  "user_id"
     t.integer  "rating"
     t.boolean  "pro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
