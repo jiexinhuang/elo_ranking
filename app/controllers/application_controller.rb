0d57b07f491e9d0a0b97e6d430acc7586ecf86fb
Jiexin Huang <jiexin.huang@envato.com>
2016-07-09 21:09:00 +0930

    Add heading partial, and fake signed_in_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_user_signed_in
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def ensure_user_signed_in
+    @signed_in_user = User.find(1)
+  end
 end
