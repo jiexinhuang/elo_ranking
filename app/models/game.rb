cd746c9e5f1cb2c0f2c3e0561fd35fae4bc05b24
Jiexin Huang <jiexin.huang@envato.com>
2016-07-09 11:00:18 +0930

    Calculate expected result of a game

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,22 @@ class Game < ApplicationRecord
   validates :result, presence: true
   validates :home_player, presence: true
   validates :away_player, presence: true
+
+  # See http://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details for more details
+  def expected_result
+    1.0 / ( 1.0 + 10.0 ** relative_rating_gap)
+  end
+
+  private
+
+  # TODO: Rename this constant
+  RATING_CONST = 400.0
+
+  def relative_rating_gap
+    rating_gap / RATING_CONST
+  end
+
+  def rating_gap
+    away_player.rating - home_player.rating
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,4 +7,41 @@ RSpec.describe Game, :type => :model do
   it { is_expected.to validate_presence_of :result }
   it { is_expected.to validate_presence_of :home_player }
   it { is_expected.to validate_presence_of :away_player }
+
+  describe '#expected_result' do
+    let(:home_player) { Player.new(rating: home_rating) }
+    let(:away_player) { Player.new(rating: away_rating) }
+    let(:game) { Game.new }
+    subject(:expected_result) { game.expected_result }
+
+    before do
+      game.home_player = home_player
+      game.away_player = away_player
+    end
+
+    context 'when both players have the same rating' do
+      let(:home_rating) { 1000 }
+      let(:away_rating) { 1000 }
+
+      it { is_expected.to eq 0.5 }
+    end
+
+    context 'when the home player rating is 1000 more than away player' do
+      let(:home_rating) { 2000 }
+      let(:away_rating) { 1000 }
+
+      it 'home player can win more than 99 times out of 100 games' do
+        expect(expected_result > 0.99).to eq true
+      end
+    end
+
+    context 'when the home player rating is 1000 less than away player' do
+      let(:home_rating) { 2000 }
+      let(:away_rating) { 3000 }
+
+      it 'home player can win less than 1 time out of 100 games' do
+        expect(expected_result < 0.01).to eq true
+      end
+    end
+  end
 end
